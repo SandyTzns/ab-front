23d9848ea45e68295414679d4ac60101770f0ddb
SandyTzns <121861083+SandyTzns@users.noreply.github.com>
2024-04-19 11:57:01 +0200

    deleted useless files

--- src/service/getData.js
+++
@@ -1,8 +0,0 @@
-import apiCalls from "./apiCalls";
-
-const exportedApi = apiCalls;
-
-export const getData = async ({ email, password }) => {
-  let data = await exportedApi.getToken({ email, password });
-  return data;
-};
